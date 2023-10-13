db04109e5a8376c335f45813551f96b7d499bd4f
Macbookpro <macbookpro@Macbookproui-MacBookPro.local>
2023-10-13 10:01:14 +0900

    구구단 함수

---
+++ index.js
@@ -0,0 +1,8 @@
+//  구구단 출력
+const makeMultiplication = () => {
+    for (let i = j; i <=9; i++) {
+         for (let j = i; j <= 9; j++){
+             console.log('${i} * ${j} = ${i*j}') ;
+    }
+    }
+}
